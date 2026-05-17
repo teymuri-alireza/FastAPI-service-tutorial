521428b43647c6933f87894acc7b85fc03e27430
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-05-17 15:46:46 +0330

    feat: add Expense class for managing expenses

--- src/main.py
+++ src/main.py
@@ -17,6 +17,35 @@ expenses_db = [
     {"id": 5, "description": "repair charger", "amount": 55.50},
 ]
 
+class Expense:
+    """
+    Represent an expense model.
+    """
+    def __init__(self, description: str, amount: float) -> None:
+        """
+        initialize the expense model.
+        """
+        self.description = description
+        self.amount = amount
+    
+    def insert(self) -> int:
+        """
+        Calculate the new index and add the new expense to the database.
+
+        Returns:
+            int: Index of the new created item.
+        """
+        new_id = expenses_db[-1]["id"] + 1
+        new_expense = {
+            "id": new_id,
+            "description": self.description,
+            "amount": self.amount,
+        }
+        expenses_db.append(new_expense)
+
+        new_index = new_id - 1
+        return new_index
+
 @app.get("/expenses")
 async def list_expenses(item_id: int | None = Query(default=None, alias="id")):
     if item_id is not None:

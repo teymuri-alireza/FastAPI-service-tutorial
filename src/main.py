f6c03e20911dfb69ca0f8ec9b4f786ef790fad39
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-05-17 15:23:07 +0330

    feat: add sample dictionary database

--- src/main.py
+++ src/main.py
@@ -7,3 +7,11 @@ patch_fastapi(app, redirect_from_root_to_docs=False)
 @app.get("/")
 async def root():
     return {"response": "Hello World"}
+
+expenses_db = [
+    {"id": 1, "description": "buy food", "amount": 12.45},
+    {"id": 2, "description": "phone bill", "amount": 18.75},
+    {"id": 3, "description": "pay tuition", "amount": 225.00},
+    {"id": 4, "description": "buy hat", "amount": 48.99},
+    {"id": 5, "description": "repair charger", "amount": 55.50},
+]

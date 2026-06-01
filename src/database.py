1ade3a45d5d285bd1f3fd77218b19dd2981a489e
teymuri-alireza <teymuri.alireza98@gmail.com>
2026-06-01 20:19:21 +0330

    feat: implement User table class in database

--- src/database.py
+++ src/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 database = "sqlite.db"
@@ -17,5 +17,16 @@ class Base(DeclarativeBase):
     Used as the base for new declarative mappings
     """
     pass
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    age = Column(Integer)
+
+    def __repr__(self):
+        return f"User(id={self.id},first_name={self.first_name},last_name={self.last_name},age={self.age})"
 
 Base.metadata.create_all(engine)
